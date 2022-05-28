8492d786db3517622d9866614c201d5bd061747b
Logesh R <logesh45@gmail.com>
2022-05-28 11:08:05 +0530

    Added routing and mongoDB Added create and get apis Added logging configuration

---
+++ src/api/user_api.rs
@@ -0,0 +1,38 @@
+use crate::{
+    models::user_model::User,
+    repository::mongodb_repo::MongoRepo,
+};
+
+use actix_web::{
+    post,get,
+    web::{Data, Json, Path},
+    HttpResponse,
+};
+
+#[post("/user")]
+pub async fn create_user(db: Data<MongoRepo>, new_user: Json<User>) -> HttpResponse {
+    let data = User {
+        id: None,
+        name: new_user.name.to_owned(),
+        location: new_user.location.to_owned(),
+        title: new_user.title.to_owned(),
+    };
+    let user_detail = db.create_user(data);
+    match user_detail {
+        Ok(user) => HttpResponse::Ok().json(user),
+        Err(err) => HttpResponse::InternalServerError().body(err.to_string()),
+    }
+}
+
+#[get("/user/{id}")]
+pub async fn get_user(db: Data<MongoRepo>, path: Path<String>) -> HttpResponse {
+    let id = path.into_inner();
+    if id.is_empty() {
+        return HttpResponse::BadRequest().body("invalid ID");
+    }
+    let user_detail = db.get_user(&id);
+    match user_detail {
+        Ok(user) => HttpResponse::Ok().json(user),
+        Err(err) => HttpResponse::InternalServerError().body(err.to_string()),
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,25 @@
-use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+mod api;
+mod models;
+mod repository;
+
+use actix_web::{
+    get,
+    App,
+    HttpResponse,
+    HttpServer,
+    Responder,
+    web::Data,
+};
+use crate::api::user_api::{create_user, get_user};
+use crate::repository::mongodb_repo::MongoRepo;
+
+#[macro_use]
+extern crate log;
+extern crate simplelog;
+
+use simplelog::*;
+
+use std::fs::File;
 
 #[get("/")]
 async fn hello() -> impl Responder {
@@ -7,9 +28,42 @@ async fn hello() -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(hello))
+    CombinedLogger::init(
+        vec![
+            TermLogger::new(
+                LevelFilter::Info,
+                Config::default(),
+                TerminalMode::Mixed,
+                ColorChoice::Auto,
+            ),
+            WriteLogger::new(
+                LevelFilter::Info,
+                Config::default(),
+                File::create("logs/app.log").unwrap()
+            ),
+            WriteLogger::new(
+                LevelFilter::Debug,
+                Config::default(),
+                File::create("logs/debug.log").unwrap()
+            ),
+        ]
+    ).unwrap();
+
+    // error!("Bright red error");
+    // debug!("This level is currently not enabled for any logger");
+
+    info!("Connecting to MongoDB");
+    let db = MongoRepo::init();
+    let db_data = Data::new(db);
+
+    HttpServer::new(move || {
+        App::new()
+            .app_data(db_data.clone())
+            .service(create_user)
+            .service(get_user)
+            .service(hello)
+    })
         .bind(("localhost", 3000))?
         .run()
         .await
-
 }
